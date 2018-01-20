ce08e07c005dcfcf9e71edb69b68a558537cb543
Hider Wang <724598894@qq.com>
2018-01-20 16:24:09 +0800

    pc版影视前台

---
+++ web/src/components/Home.vue
@@ -0,0 +1,64 @@
+<template>
+    <div>
+        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
+            <el-menu-item index="1">首页</el-menu-item>
+            <el-menu-item index="2">电影</el-menu-item>
+            <el-menu-item index="3">电视剧</el-menu-item>
+            <el-menu-item index="4">综艺</el-menu-item>
+            <el-menu-item index="5">动漫</el-menu-item>
+            <el-menu-item index="6">vip解析</el-menu-item>
+        </el-menu>
+
+        <el-row :gutter="20">
+            <el-col :span="16">
+                <div class="grid-content bg-purple">
+                    <el-carousel indicator-position="outside">
+                        <el-carousel-item v-for="item in 4" :key="item">
+                            <h3>{{ item }}</h3>
+                        </el-carousel-item>
+                    </el-carousel>
+                </div>
+            </el-col>
+            <el-col :span="8">
+                <div class="grid-content bg-purple">
+
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "home",
+        data() {
+            return {
+                activeIndex: '1',
+                activeIndex2: '1'
+            };
+        },
+        methods: {
+            handleSelect(key, keyPath) {
+                console.log(key, keyPath);
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 18px;
+        opacity: 0.75;
+        line-height: 300px;
+        margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
+</style>
